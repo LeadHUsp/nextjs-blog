0f5ab9c44d661cdd2db3f60e76cb5db78cb09d2f
LeadHUsp <bodvinukr@gmail.com>
2020-10-27 19:38:38 +0300

    deploy herolku

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -34,7 +34,7 @@ export async function getServerSideProps(ctx) {
   // Call an external API endpoint to get posts.
   // You can use any data fetching library
   const res = await fetch(
-    `http://blog.ssr/wp-json/wp/v2/posts?per_page=3&page=${
+    `https://react-base.000webhostapp.com/wp-json/wp/v2/posts?per_page=3&page=${
       ctx.params ? ctx.params.page : 1
     }`
   );
