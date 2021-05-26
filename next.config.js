5efe07cffa8ce7b911a19c4ffc024e86907a4362
LeadHUsp <bodvinukr@gmail.com>
2021-05-26 22:31:43 +0300

    change api server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        api_key: 'https://portfolio-serv.000webhostapp.com/wp-json/wp/v2',
+        api_key: 'http://f0545652.xsph.ru/wp-json/wp/v2',
         api_contact_form: 'https://api.emailjs.com/api/v1.0/email/send',
         email_services_id: 'service_n8f79ye',
         email_template_id: 'template_w1wgbhr',
