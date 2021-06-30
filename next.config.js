265d99f49b4ff2a7b651fd5504b95f5fe924e25e
LeadHUsp <bodvinukr@gmail.com>
2021-06-30 14:13:11 +0300

    started new branch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        api_key: 'https://portfolio-serv.000webhostapp.com/wp-json/wp/v2',
+        api_key: 'http://localhost:1337/',
         api_contact_form: 'https://api.emailjs.com/api/v1.0/email/send',
         email_services_id: 'service_n8f79ye',
         email_template_id: 'template_w1wgbhr',
